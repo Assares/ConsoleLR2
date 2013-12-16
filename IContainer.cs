6f9a5fd5e63698fca1c6d2105c199cbb0c9bc389
Valentine <Scum@i.ua>
2013-12-16 19:00:11 +0200

    Valentins commit

---
+++ IContainer.cs
@@ -0,0 +1,44 @@
+﻿/// <copyright file="IContainer.cs"company="VS">
+/// Все права защищены
+/// </copyright>
+namespace WindowsFormsApplication1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    /// <summary>
+    /// Интерфейс контейнера
+    /// </summary>
+    /// <typeparam name="TElement"> Тип </typeparam>
+    interface IContainer<TElement>
+    {
+        /// <summary>
+        /// Получение количества элементов в контейнере
+        /// </summary>
+        Int32 Count
+        {
+            get;
+        }
+        /// <summary>
+        /// Перегрузка квадратных скобок
+        /// </summary>
+        /// <param name="index"> Индекс элемента в контейнере </param>
+        /// <returns> Элемента контейнера </returns>
+        TElement this[int index]
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Добавление элемента
+        /// </summary>
+        /// <param name="element)"> Элемент на добавление </param>
+        void Add(TElement element);
+        /// <summary>
+        /// Удаление элемента 
+        /// </summary>
+        /// <param name="element)"> Элемент на удаление </param>
+        void Delete(TElement element);
+    }
+}
---
+++ Planet.cs
@@ -0,0 +1,97 @@
+﻿/// <copyright file="Planet.cs"company="VS">
+/// Все права защищены
+/// </copyright>
+namespace WindowsFormsApplication1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    /// <summary>
+    /// Клас планета
+    /// </summary>
+    class Planet
+    {
+        /// <summary>
+        /// Имя Планеты
+        /// </summary>
+        private string name;
+        /// <summary>
+        /// Размер планеты
+        /// </summary>
+        private int size;
+        /// <summary>
+        /// Есть ли спутник у планеты
+        /// </summary>
+        private bool sattelite;
+	    /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Planet"/>
+        /// </summary>
+        public Planet() 
+        {
+        }
+	    
+	/// <param name="weight"> Имя планеты </param>
+	/// <param name="numberOfPlanets"> Размер планет </param>
+	/// <param name="life"> Спутник </param>
+        public Planet(string name, int size, bool sattelite) 
+        {
+            this.name = name;
+            this.size = size;
+            this.sattelite = sattelite;
+        }
+        /// <summary>
+        /// Получает или задает Имя планеты
+        /// </summary>
+        public string Name 
+        {
+            get 
+            {
+                return this.name;
+            }
+            
+            set 
+            {
+                this.name = value;
+            }
+        }
+        /// <summary>
+        /// Получает или задает Размер планет
+        /// </summary>
+        public int Size 
+        {
+            get 
+            {
+                return this.size;
+            }
+
+            set 
+            {
+                this.size = value;
+            }
+        }
+        /// <summary>
+        /// Получает или задает наличие спутника у планеты
+        /// </summary>
+        public bool Sattelite 
+        {
+            get 
+            {
+                return this.sattelite;
+            }
+
+            set 
+            {
+                this.sattelite = value;
+            }
+        }
+        /// <summary>
+        /// Перегрузка метода
+        /// </summary>	
+	/// <returns> Строка с описанием Планеты </returns>
+        public override string ToString() 
+        {
+            return this.name.ToString() + " " + this.size.ToString() + " " + (this.sattelite ? "True" : "False");
+        }
+    }    
+}
